1ee0c5f0910f8e49c1e0acca9b67dc5b1e252009
CK-Tang0413 <tterry616@gmail.com>
2026-03-31 13:28:17 +0800

    try fix pull error

--- src/main.java
+++ src/main.java
@@ -5,10 +5,10 @@ import panes.CustomerMainPane;
 import panes.ManagerMainPane;
 
 public class main{
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		SwingUtilities.invokeLater(() -> {
-//			ManagerMainPane testPane = new ManagerMainPane();
-			CustomerMainPane testPane = new CustomerMainPane();
+			ManagerMainPane testPane = new ManagerMainPane();
+//			CustomerMainPane testPane = new CustomerMainPane();
 			testPane.setVisible(true);
 		});
 	}
