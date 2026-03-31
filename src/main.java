9cefee110b7de8468f7d92148fb1cd7530c77a05
Timny-NCS <chongshengtimothy@gmail.com>
2026-03-31 13:32:32 +0800

    Trying solving bugs
    Testing

--- src/main.java
+++ src/main.java
@@ -3,12 +3,14 @@ import java.awt.*;
 
 import panes.CustomerMainPane;
 import panes.ManagerMainPane;
+import panes.TechnicianMainPane;
 
 public class main{
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 //			ManagerMainPane testPane = new ManagerMainPane();
-			CustomerMainPane testPane = new CustomerMainPane();
+//			CustomerMainPane testPane = new CustomerMainPane();
+			TechnicianMainPane testPane = new TechnicianMainPane();
 			testPane.setVisible(true);
 		});
 	}
