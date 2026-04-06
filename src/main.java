fb9a3f8190055edfbf45614d90dcfcb1116169cc
Timny-NCS <chongshengtimothy@gmail.com>
2026-04-06 13:53:17 +0800

    Update Techician main pane into main
    Update technician pane

--- src/main.java
+++ src/main.java
@@ -3,13 +3,15 @@ import javax.swing.*;
 import panes.CounterStaff.CounterStaffMainPane;
 import panes.Customer.CustomerMainPane;
 import panes.Manager.ManagerMainPane;
+import panes.Techinician.TechnicianMainPane;
 
 public class main{
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 //			 ManagerMainPane testPane = new ManagerMainPane();
-			 CustomerMainPane testPane = new CustomerMainPane();
+//			 CustomerMainPane testPane = new CustomerMainPane();
 //			CounterStaffMainPane testPane = new CounterStaffMainPane();
+			TechnicianMainPane testPane = new TechnicianMainPane();
 			testPane.setVisible(true);
 		});
 	}
