cbe66ba0f609fa201fed0a94e91130afa44905e1
Timny-NCS <chongshengtimothy@gmail.com>
2026-04-22 16:40:59 +0800

    adding path for scraping file data
    add file path

--- src/IO/FileHandler.java
+++ src/IO/FileHandler.java
@@ -14,36 +14,36 @@ public class FileHandler {
 
     // }
 
-    // public static void write(String filename, ArrayList<String> data){
-    //     try {
-    //         BufferedWriter bw = new BufferedWriter(new FileWriter(defaultDataFilePath + filename, true));
-    //             for(int i = 0; i < data.size(); i++){
-    //                 if(i != data.size() - 1){
-    //                     bw.write(data.get(i) + separator);
-    //                 } else{
-    //                     bw.write(data.get(i) + "\n");
-    //                 }
-    //             }
-    //         bw.close();
-            
-    //     } catch (IOException e) {
-    //         System.err.println("Error writing to file: " + defaultDataFilePath + filename);
-    //         e.printStackTrace();
-    //     }
-    // }
+//     public static void write(String filename, ArrayList<String> data){
+//         try {
+//             BufferedWriter bw = new BufferedWriter(new FileWriter("src/Databases/" + filename, true));
+//                 for(int i = 0; i < data.size(); i++){
+//                     if(i != data.size() - 1){
+//                         bw.write(data.get(i) + ",");
+//                     } else{
+//                         bw.write(data.get(i) + "\n");
+//                     }
+//                 }
+//             bw.close();
+//
+//         } catch (IOException e) {
+//             System.err.println("Error writing to file: " + "src/Databases/" + filename);
+//             e.printStackTrace();
+//         }
+//     }
 
-    // public static ArrayList read(String filename){
-
-    //     ArrayList<String[]> outputList = new ArrayList<String[]>();
-
-    //     try {
-    //         String line = "";
-    //         BufferedReader br = new BufferedReader(new FileReader(defaultDataFilePath + filename));
-    //         while ((line = br.readLine()) != null){
-    //             String[] list = line.split(separator);
-    //         }
-    //     } catch(IOException e){
-    //         System.err.println("Error reading file: " + defaultDataFilePath + filename);
-    //     }
-    // }
+//     public static ArrayList read(String filename){
+//
+//         ArrayList<String[]> outputList = new ArrayList<String[]>();
+//
+//         try {
+//             String line = "";
+//             BufferedReader br = new BufferedReader(new FileReader(defaultDataFilePath + filename));
+//             while ((line = br.readLine()) != null){
+//                 String[] list = line.split(separator);
+//             }
+//         } catch(IOException e){
+//             System.err.println("Error reading file: " + defaultDataFilePath + filename);
+//         }
+//     }
 }
